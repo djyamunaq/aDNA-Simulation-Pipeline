9eca6af20b70071284c6bbaffb714d6ba5103312
djyamunaq <dy.xcvi@gmail.com>
2023-12-08 19:19:35 -0300

    Added gargammel submodule

--- install.sh
+++ install.sh
@@ -1,2 +1,8 @@
-make ./sim/gargammel/
+sudo apt update
+sudo apt install cmake
+sudo apt-get install libgsl0-dev
+sudo apt install seq-gen
+cd sim/gargammel
+make
+cd -
 pip install -e .
--- sim/__main__.py
+++ sim/__main__.py
@@ -15,16 +15,16 @@ def main():
     # Clear previous endo data
     subprocess.run(['rm', '-rf', ])
     # Move data to default location
-    subprocess.run(['cp', args.refDNA, os.path.join(os.path.dirname(__file__), './gargammel/data/endo/')])
+    subprocess.run(['cp', args.refDNA, os.path.join(os.path.dirname(__file__), './.data/endo/')])
     # Run gargammel simulation
-    subprocess.run([os.path.join(os.path.dirname(__file__),'./gargammel/gargammel.pl'), os.path.join(os.path.dirname(__file__), './gargammel/data/')])
+    subprocess.run([os.path.join(os.path.dirname(__file__),'./gargammel/gargammel.pl'), os.path.join(os.path.dirname(__file__), './.data/')])
     # Remove output dir if it exists
     subprocess.run(['rm', '-rf', args.output])
     # Create output
     subprocess.run(['mkdir', args.output])
     # Move files to output dir
-    subprocess.run(['cp', os.path.join(os.path.dirname(__file__), './gargammel/data/simadna_s1.fq.gz'), os.path.join(args.output, 'simadna_s1.fq.gz')])
-    subprocess.run(['cp', os.path.join(os.path.dirname(__file__), './gargammel/data/simadna_s2.fq.gz'), os.path.join(args.output, 'simadna_s2.fq.gz')])
+    subprocess.run(['cp', os.path.join(os.path.dirname(__file__), './.data/simadna_s1.fq.gz'), os.path.join(args.output, 'simadna_s1.fq.gz')])
+    subprocess.run(['cp', os.path.join(os.path.dirname(__file__), './.data/simadna_s2.fq.gz'), os.path.join(args.output, 'simadna_s2.fq.gz')])
     # Decompress files and delete previous compressed files
     subprocess.run(['gzip', '-d', '-q', '-f', os.path.join(args.output, 'simadna_s1.fq.gz')])
     subprocess.run(['gzip', '-d', '-q', '-f', os.path.join(args.output, 'simadna_s2.fq.gz')])
